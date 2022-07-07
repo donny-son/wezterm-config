a042273822942d44972f05ff642911e60060aced
Dongook Son <60206749+donny-son@users.noreply.github.com>
2022-07-08 00:01:55 +0900

    Init

---
+++ wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require 'wezterm';
+return {
+  font = wezterm.font({
+    family = "FantasqueSansMono Nerd Font Mono",
+  }),
+  font_size = 17,
+  color_scheme = "Batman",
+  default_cursor_style = "BlinkingBlock",
+  cursor_blink_ease_in = "Linear",
+  cursor_blink_ease_out = "Linear",
+  cursor_blink_rate = 500,
+  animation_fps = 60,
+  audible_bell = "Disabled",
+  enable_tab_bar = false,
+  line_height = 1.2,
+}
